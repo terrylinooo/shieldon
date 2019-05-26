3cbc298a9b20bdcf9ff423c219e006227755a230
Terry Lin <terry@fontrip.com>
2019-05-27 01:37:36 +0800

    Add a way for injecting CSRF fields into Captcha form.

---
+++ src/Shieldon/Captcha/Csrf.php
@@ -0,0 +1,68 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of the Shieldon package.
+ *
+ * (c) Terry L. <contact@terryl.in>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Shieldon\Captcha;
+
+class Csrf implements CaptchaInterface
+{
+    /**
+     * Form input name.
+     *
+     * @var string
+     */
+    protected $name = '';
+
+    /**
+     * Form input value.
+     *
+     * @var string
+     */
+    protected $value = '';
+
+    /**
+     * Constructor.
+     * 
+     * It will implement default configuration settings here.
+     * 
+     * @array $config
+     *
+     * @return void
+     */
+    public function __construct(array $config = [])
+    {
+        foreach ($config as $k => $v) {
+            if (isset($this->{$k})) {
+                $this->{$k} = $v;
+            }
+        }
+    }
+
+    /**
+     * Reponse the result.
+     *
+     * @return bool
+     */
+    public function response(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Output a required HTML.
+     *
+     * @return string
+     */
+    public function form(): string
+    {
+        $html = '<input type="hidden" name="' . $this->name . '" value="' . $this->value . '">';
+
+        return $html;
+    }
+}
