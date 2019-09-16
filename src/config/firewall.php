9522ad97712b339a7f9d9658ecae0179b024ea00
Terry Lin <terry@fontrip.com>
2019-09-16 15:24:58 +0800

    Update configuration sample files.

--- src/config/firewall.php
+++ src/config/firewall.php
@@ -4,25 +4,22 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Application Name
+    | Deamon
     |--------------------------------------------------------------------------
     |
-    | This value is the name of your application. This value is used when the
-    | framework needs to place the application's name in a notification or
-    | any other location as required by the application or its packages.
+    | Setting true to this value to start Shieldon running on the background.
+    | Every HTTP request will be analyzed.
     |
     */
 
-    'daemon' => true,
+    'daemon' => false,
 
     /*
     |--------------------------------------------------------------------------
-    | Application Environment
+    | Channel Id
     |--------------------------------------------------------------------------
     |
-    | This value determines the "environment" your application is currently
-    | running in. This may determine how you prefer to configure various
-    | services the application utilizes. Set this in your ".env" file.
+    | If you would like use mutliple Shieldon, specifying a Channel Id here.
     |
     */
 
@@ -30,202 +27,356 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Application Debug Mode
+    | Driver Type
     |--------------------------------------------------------------------------
     |
-    | When your application is in debug mode, detailed error messages with
-    | stack traces will be shown on every error that occurs within your
-    | application. If disabled, a simple generic error page is shown.
+    | The type of the Data driver
     |
     */
 
-    'drivers' => env('APP_DEBUG', false),
+    'driver_type' => 'file',
 
     /*
     |--------------------------------------------------------------------------
-    | Application URL
+    | Drivers
     |--------------------------------------------------------------------------
     |
-    | This URL is used by the console to properly generate URLs when using
-    | the Artisan command line tool. You should set this to the root of
-    | your application so that it is used when running Artisan tasks.
+    | The setting details of the data drivers.
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'drivers' => [
 
-    'asset_url' => env('ASSET_URL', null),
+        // Data driver: File system.
+        'file' => [
+            'enable' => true,
+            'config' => [
+                'directory_path' => '',
+            ],
+        ],
 
-    /*
-    |--------------------------------------------------------------------------
-    | Application Timezone
-    |--------------------------------------------------------------------------
-    |
-    | Here you may specify the default timezone for your application, which
-    | will be used by the PHP date and date-time functions. We have gone
-    | ahead and set this to a sensible default for you out of the box.
-    |
-    */
+        // Data driver: File system.
+        'mysql' => [
+            'enable' => true,
+            'config' => [
+                'host'    => '127.0.0.1',
+                'dbname'  => 'shieldon_db',
+                'user'    => 'shieldon_user',
+                'pass'    => '1234',
+                'charset' => 'utf8',
+            ],
+        ],
 
-    'timezone' => 'UTC',
-
-    /*
-    |--------------------------------------------------------------------------
-    | Application Locale Configuration
-    |--------------------------------------------------------------------------
-    |
-    | The application locale determines the default locale that will be used
-    | by the translation service provider. You are free to set this value
-    | to any of the locales which will be supported by the application.
-    |
-    */
-
-    'locale' => 'en',
-
-    /*
-    |--------------------------------------------------------------------------
-    | Application Fallback Locale
-    |--------------------------------------------------------------------------
-    |
-    | The fallback locale determines the locale to use when the current one
-    | is not available. You may change the value to correspond to any of
-    | the language folders that are provided through your application.
-    |
-    */
-
-    'fallback_locale' => 'en',
-
-    /*
-    |--------------------------------------------------------------------------
-    | Faker Locale
-    |--------------------------------------------------------------------------
-    |
-    | This locale will be used by the Faker PHP library when generating fake
-    | data for your database seeds. For example, this will be used to get
-    | localized telephone numbers, street address information and more.
-    |
-    */
-
-    'faker_locale' => 'en_US',
-
-    /*
-    |--------------------------------------------------------------------------
-    | Encryption Key
-    |--------------------------------------------------------------------------
-    |
-    | This key is used by the Illuminate encrypter service and should be set
-    | to a random, 32 character string, otherwise these encrypted strings
-    | will not be safe. Please do this before deploying an application!
-    |
-    */
-
-    'key' => env('APP_KEY'),
-
-    'cipher' => 'AES-256-CBC',
-
-    /*
-    |--------------------------------------------------------------------------
-    | Autoloaded Service Providers
-    |--------------------------------------------------------------------------
-    |
-    | The service providers listed here will be automatically loaded on the
-    | request to your application. Feel free to add your own services to
-    | this array to grant expanded functionality to your applications.
-    |
-    */
-
-    'providers' => [
-
-        /*
-         * Laravel Framework Service Providers...
-         */
-        Illuminate\Auth\AuthServiceProvider::class,
-        Illuminate\Broadcasting\BroadcastServiceProvider::class,
-        Illuminate\Bus\BusServiceProvider::class,
-        Illuminate\Cache\CacheServiceProvider::class,
-        Illuminate\Foundation\Providers\ConsoleSupportServiceProvider::class,
-        Illuminate\Cookie\CookieServiceProvider::class,
-        Illuminate\Database\DatabaseServiceProvider::class,
-        Illuminate\Encryption\EncryptionServiceProvider::class,
-        Illuminate\Filesystem\FilesystemServiceProvider::class,
-        Illuminate\Foundation\Providers\FoundationServiceProvider::class,
-        Illuminate\Hashing\HashServiceProvider::class,
-        Illuminate\Mail\MailServiceProvider::class,
-        Illuminate\Notifications\NotificationServiceProvider::class,
-        Illuminate\Pagination\PaginationServiceProvider::class,
-        Illuminate\Pipeline\PipelineServiceProvider::class,
-        Illuminate\Queue\QueueServiceProvider::class,
-        Illuminate\Redis\RedisServiceProvider::class,
-        Illuminate\Auth\Passwords\PasswordResetServiceProvider::class,
-        Illuminate\Session\SessionServiceProvider::class,
-        Illuminate\Translation\TranslationServiceProvider::class,
-        Illuminate\Validation\ValidationServiceProvider::class,
-        Illuminate\View\ViewServiceProvider::class,
-
-        /*
-         * Package Service Providers...
-         */
-
-        /*
-         * Application Service Providers...
-         */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
-
+        // Data driver: SQLite.
+        'sqlite' => [
+            'enable' => true,
+            'config' => [
+                'directory_path' => '',
+            ],
+        ],
     ],
 
     /*
     |--------------------------------------------------------------------------
-    | Class Aliases
+    | Loggers
     |--------------------------------------------------------------------------
     |
-    | This array of class aliases will be registered when this application
-    | is started. However, feel free to register as many as you wish as
-    | the aliases are "lazy" loaded so they don't hinder performance.
+    | Logging data to the logs files then we can parse them to visual charts.
+    | Currently, we provide only Action Logger now.
     |
     */
 
-    'aliases' => [
+    'loggers' => [
+        'action' => [
+            'enable' => false,
+            'config' => [
+                'directory_path' => '',
+            ],
+        ],
+    ],
 
-        'App' => Illuminate\Support\Facades\App::class,
-        'Arr' => Illuminate\Support\Arr::class,
-        'Artisan' => Illuminate\Support\Facades\Artisan::class,
-        'Auth' => Illuminate\Support\Facades\Auth::class,
-        'Blade' => Illuminate\Support\Facades\Blade::class,
-        'Broadcast' => Illuminate\Support\Facades\Broadcast::class,
-        'Bus' => Illuminate\Support\Facades\Bus::class,
-        'Cache' => Illuminate\Support\Facades\Cache::class,
-        'Config' => Illuminate\Support\Facades\Config::class,
-        'Cookie' => Illuminate\Support\Facades\Cookie::class,
-        'Crypt' => Illuminate\Support\Facades\Crypt::class,
-        'DB' => Illuminate\Support\Facades\DB::class,
-        'Eloquent' => Illuminate\Database\Eloquent\Model::class,
-        'Event' => Illuminate\Support\Facades\Event::class,
-        'File' => Illuminate\Support\Facades\File::class,
-        'Gate' => Illuminate\Support\Facades\Gate::class,
-        'Hash' => Illuminate\Support\Facades\Hash::class,
-        'Lang' => Illuminate\Support\Facades\Lang::class,
-        'Log' => Illuminate\Support\Facades\Log::class,
-        'Mail' => Illuminate\Support\Facades\Mail::class,
-        'Notification' => Illuminate\Support\Facades\Notification::class,
-        'Password' => Illuminate\Support\Facades\Password::class,
-        'Queue' => Illuminate\Support\Facades\Queue::class,
-        'Redirect' => Illuminate\Support\Facades\Redirect::class,
-        'Redis' => Illuminate\Support\Facades\Redis::class,
-        'Request' => Illuminate\Support\Facades\Request::class,
-        'Response' => Illuminate\Support\Facades\Response::class,
-        'Route' => Illuminate\Support\Facades\Route::class,
-        'Schema' => Illuminate\Support\Facades\Schema::class,
-        'Session' => Illuminate\Support\Facades\Session::class,
-        'Storage' => Illuminate\Support\Facades\Storage::class,
-        'Str' => Illuminate\Support\Str::class,
-        'URL' => Illuminate\Support\Facades\URL::class,
-        'Validator' => Illuminate\Support\Facades\Validator::class,
-        'View' => Illuminate\Support\Facades\View::class,
+    /*
+    |--------------------------------------------------------------------------
+    | Filters
+    |--------------------------------------------------------------------------
+    |
+    | Filters are the soft rule-sets to detect bad-behavior requests then we 
+    | can "temporarily" ban them. (Unbannend by solving CAPTCHA by themselves.)
+    |
+    */
 
+    'filters' => [
+
+        // Fequency filter.
+        'frequency' => [
+            'enable' => false,
+            'config' => [
+                'quota_s' => 2,
+                'quota_m' => 10,
+                'quota_h' => 30,
+                'quota_d' => 60,
+            ],
+        ],
+
+        // JavaScript cookie filter.
+        'cookie' => [
+            'enable' => false,
+            'config' => [
+                'cookie_name'   => 'ssjd',
+				'cookie_domain' => '',
+				'cookie_value'  => '1',
+				'quota'         => 5,
+            ],
+        ],
+
+        // Session filter.
+        'session' => [
+            'enable' => false,
+            'config' => [
+                'quota'       => 5,
+                'time_buffer' => 5,
+            ],
+        ],
+
+        // Referer filter.
+        'referer' => [
+            'enable' => false,
+            'config' => [
+                'quota'       => 5,
+                'time_buffer' => 5,
+            ],
+        ],
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Components
+    |--------------------------------------------------------------------------
+    |
+    | Components are the hard rule-sets to detect bad-behavior requests then we 
+    | can "permanently" ban them. (No CAPTCHA shows.)
+    |
+    | Each component provides its public APIs for further control.
+    |
+    */
+
+    'components' => [
+
+        // IP component.
+        'ip' => [
+            'enable' => true,
+        ],
+
+        // Trusted-bot component.
+        'trusted_bot' => [
+            'enable'      => true,
+            'strict_mode' => false,
+        ],
+
+        // Header filter.
+        'header' => [
+            'enable'      => true,
+            'strict_mode' => false,
+        ],
+
+        // User-agent filter.
+        'user_agent' => [
+            'enable'      => true,
+            'strict_mode' => false,
+        ],
+
+        // RDNS filter.
+        'rdns' => [
+            'enable'      => true,
+            'strict_mode' => false,
+        ],
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | CAPTCHA Modules
+    |--------------------------------------------------------------------------
+    |
+    | CAPTCHA modules provide its way to unban users.
+    |
+    */
+
+	'captcha_modules' => [
+
+        // Google reCAPTCHA.
+		'recaptcha' => [
+			'enable' => false,
+			'config' => [
+				'site_key'   => null,
+				'secret_key' => null,
+				'version'    => 'v2',
+				'lang'       => 'en-US'
+            ],
+        ],
+
+        // A very simple image CAPTCHA.
+        'image' => [
+			'enable' => false,
+			'config' => [
+				'type'   =>  'alnum',
+				'length' => 4
+            ],
+        ],
+    ],
+    
+    /*
+    |--------------------------------------------------------------------------
+    | IP Variable Source
+    |--------------------------------------------------------------------------
+    |
+    | Picking up the real IP source is a must if you use CDN service on frontend.
+    |
+    */
+
+    'ip_variable_source' => [
+        'REMOTE_ADDR'           => true,
+		'HTTP_CF_CONNECTING_IP' =>  false,
+		'HTTP_X_FORWARDED_FOR'  =>  false,
+		'HTTP_X_FORWARDED_HOST' =>  false
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Online Session Limit
+    |--------------------------------------------------------------------------
+    |
+    | When the online user amount has reached the limit, other users not in the 
+    | queue have to line up!
+    |
+    */
+
+    'online_session_limit' => [
+        'enable' => false,
+        'config' => [
+            'count'  =>  100,
+            'period' => 300,
+        ],
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | XSS protection. (Not ready.)
+    |--------------------------------------------------------------------------
+    |
+    | Googling "XSS" to understand who it is and how can we do.
+    | This feature is still under development. Give us suggestions.
+    |
+    */
+
+    'xss_protection' => [
+
+        'request_uri' => [
+            'enable' => true,
+        ],
+
+        'post' => [
+            'enable' => false,
+        ],
+
+        'get' => [
+            'enable' => false,
+        ],
+
+        'cookie' => [
+            'enable' => false,
+        ],
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | CSRF protection. (Not ready.)
+    |--------------------------------------------------------------------------
+    |
+    | Googling "CSRF" to understand who it is and how can we do.
+    | This feature is still under development. Give us suggestions.
+    |
+    */
+
+    'csrf_protection' => [
+        'enable' => true,
+        'config' => [
+            'expire'        => 7200,
+            'excluded_urls' => [
+                [
+                    'url' => '/ajax/',
+                ],
+            ],
+        ],
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cronjob
+    |--------------------------------------------------------------------------
+    |
+    | Shieldon's CRON is triggered by HTTP request, not real system CRON.
+    |
+    */
+
+    'cronjob' => [
+
+        'reset_circle' => [
+            'enable' => true,
+            'config' => [
+                'period'      => 86400,
+                'last_update' => '2019-01-01 00:00:00',
+            ],
+        ],
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Excluded URLs
+    |--------------------------------------------------------------------------
+    |
+    | Shieldon will ignore URLs listed blew.
+    |
+    */
+
+    'excluded_urls' => [
+        [
+            'url' => '/tests/',
+        ],
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | IP Manager
+    |--------------------------------------------------------------------------
+    |
+    | IP manager is provided by IP component.
+    |
+    */
+
+    'ip_manager' => [
+        [
+			'rule' => 'allow',
+			'ip'   => '127.0.0.1',
+			'url'  => '/',
+        ],
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password-protection URLs
+    |--------------------------------------------------------------------------
+    |
+    | The URLs below are protected by password.
+    |
+    */
+
+    'ip_manager' => [
+        [
+            'url'      => '/',
+            'username' => 'shieldon_test',
+            'password' => 'shieldon_test',
+			'captcha'  => true,
+        ],
     ],
 
 ];
