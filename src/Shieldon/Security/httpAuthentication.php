d9a912911b2559e76a534da9627268663e9c0e20
Terry Lin <terry@fontrip.com>
2019-09-26 00:07:04 +0800

    Fix syntax error.

--- src/Shieldon/Security/httpAuthentication.php
+++ src/Shieldon/Security/httpAuthentication.php
@@ -92,7 +92,7 @@ class httpAuthentication
 
                 // Prompt a window to ask for username and password.
                 if (! isset($_SERVER['PHP_AUTH_USER']) || ! isset($_SERVER['PHP_AUTH_PW'])) {
-                    header('WWW-Authenticate: Basic realm="' . $this->realm() . '"');
+                    header('WWW-Authenticate: Basic realm="' . $this->realm . '"');
                     header('HTTP/1.0 401 Unauthorized');
                     die('Permission required.');
                 }
