97fc32b1ae7addf37842aa8d117a6055c1ee60f7
Terry Lin <terry@fontrip.com>
2020-07-20 22:22:31 +0800

    Improve code quality.

--- src/Firewall/Firewall.php
+++ src/Firewall/Firewall.php
@@ -60,6 +60,7 @@ class Firewall
 {
     use FirewallTrait;
     use XssProtectionTrait;
+    use MessengerTrait;
 
     /**
      * Collection of PSR-7 or PSR-15 middlewares.
@@ -424,69 +425,7 @@ class Firewall
         }
     }
 
-    /**
-     * Set the messenger modules.
-     *
-     * @return void
-     */
-    protected function setMessengers(): void
-    {
-        /*
-        |--------------------------------------------------------------------------
-        | Set messenger modules.
-        |--------------------------------------------------------------------------
-        */
-
-        $messengerList = [
-            'telegram',
-            'line_notify',
-            'sendgrid',
-            'native_php_mail',
-            'smtp',
-            'mailgun',
-            'rocket_chat',
-            'slack',
-            'slack_webhook',
-        ];
-
-        foreach ($messengerList as $messenger) {
-            $setting = $this->getOption($messenger, 'messengers');
-
-            if (is_array($setting)) {
-
-                // Initialize messenger instances from the factory/
-                if (MessengerFactory::check($messenger, $setting)) {
     
-                    $this->kernel->add(
-                        MessengerFactory::getInstance(
-                            // The ID of the messenger module in the configuration.
-                            $messenger, 
-                            // The settings of the messenger module in the configuration.
-                            $setting    
-                        )
-                    );
-                }
-            }
-
-            unset($setting);
-        }
-
-       /*
-        |--------------------------------------------------------------------------
-        | Set messenger events.
-        |--------------------------------------------------------------------------
-        */
-
-        $setting = $this->getOption('failed_attempts_in_a_row', 'events');
-
-        $notifyDataCircle     = $setting['data_circle']['messenger']     ?: false;
-        $notifySystemFirewall = $setting['system_firewall']['messenger'] ?: false;
-
-        $this->kernel->setProperty('deny_attempt_notify', [
-            'data_circle'     => $notifyDataCircle,
-            'system_firewall' => $notifySystemFirewall,
-        ]);
-    }
 
     /**
      * Set deny attempts.
@@ -667,4 +606,6 @@ class Firewall
             $this->kernel->setDialogUI($this->getOption('dialog_ui'));
         }
     }
+
+  
 }
