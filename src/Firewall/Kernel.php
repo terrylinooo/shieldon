4bb95ad4c201b6109ae73686bb8380d68b0b1472
Terry Lin <terry@fontrip.com>
2020-08-04 11:53:15 +0800

    Add Template trait.

--- src/Firewall/Kernel.php
+++ src/Firewall/Kernel.php
@@ -35,25 +35,20 @@ use Shieldon\Firewall\Kernel\FilterTrait;
 use Shieldon\Firewall\Kernel\MessengerTrait;
 use Shieldon\Firewall\Kernel\RuleTrait;
 use Shieldon\Firewall\Kernel\SessionTrait;
+use Shieldon\Firewall\Kernel\TemplateTrait;
 use Shieldon\Firewall\Log\ActionLogger;
 use Shieldon\Firewall\Utils\Container;
 use function Shieldon\Firewall\get_default_properties;
 use function Shieldon\Firewall\get_request;
-use function Shieldon\Firewall\get_response;
 use function Shieldon\Firewall\get_session;
 
 use Closure;
-use InvalidArgumentException;
 use RuntimeException;
 use function array_push;
 use function file_exists;
 use function get_class;
 use function gethostbyaddr;
-use function is_dir;
 use function ltrim;
-use function ob_end_clean;
-use function ob_get_contents;
-use function ob_start;
 use function strpos;
 use function strrpos;
 use function substr;
@@ -69,9 +64,7 @@ class Kernel
      *  ----------------------|---------------------------------------------
      *   ban                  | Ban an IP.
      *   getCurrentUrl        | Get current user's browsing path.
-     *   getJavascript        | Print a JavaScript snippet in the pages.
      *   managedBy            | Used on testing purpose.
-     *   respond              | Respond the result.
      *   run                  | Run the checking process.
      *   setClosure           | Set a closure function.
      *   setDialog            | Customize the dialog UI.
@@ -81,7 +74,6 @@ class Kernel
      *   setProperties        | Set the property settings.
      *   setProperty          | Set a property setting.
      *   setStrict            | Strict mode apply to all components.
-     *   setTemplateDirectory | Set the frontend template directory.
      *   unban                | Unban an IP.
      *  ----------------------|---------------------------------------------
      */
@@ -162,6 +154,16 @@ class Kernel
      *  ----------------------|---------------------------------------------
      */
     use SessionTrait;
+
+    /**
+     *   Public methods       | Desctiotion
+     *  ----------------------|---------------------------------------------
+     *   respond              | Respond the result.
+     *   setTemplateDirectory | Set the frontend template directory.
+     *   getJavascript        | Print a JavaScript snippet in the pages.
+     *  ----------------------|---------------------------------------------
+     */
+    use TemplateTrait;
 
     /**
      * HTTP Status Codes
@@ -383,118 +385,6 @@ class Kernel
     }
 
     /**
-     * Respond the result.
-     *
-     * @return ResponseInterface
-     */
-    public function respond(): ResponseInterface
-    {
-        $response = get_response();
-
-        $httpStatusCodes = [
-            self::RESPONSE_TEMPORARILY_DENY => [
-                'type' => 'captcha',
-                'code' => self::HTTP_STATUS_FORBIDDEN,
-            ],
-
-            self::RESPONSE_LIMIT_SESSION => [
-                'type' => 'session_limitation',
-                'code' => self::HTTP_STATUS_TOO_MANY_REQUESTS,
-            ],
-
-            self::RESPONSE_DENY => [
-                'type' => 'rejection',
-                'code' => self::HTTP_STATUS_BAD_REQUEST,
-            ],
-        ];
-
-        // Nothing happened. Return.
-        if (empty($httpStatusCodes[$this->result])) {
-            return $response;
-        }
-
-        $type = $httpStatusCodes[$this->result]['type'];
-        $statusCode = $httpStatusCodes[$this->result]['code'];
-
-        $viewPath = $this->getTemplate($type);
-
-        // The language of output UI. It is used on views.
-        $langCode = get_session()->get('shieldon_ui_lang') ?? 'en';
-
-        $showOnlineInformation = false;
-        $showUserInformation = false;
-        
-        // Show online session count. It is used on views.
-        if (!empty($this->properties['display_online_info'])) {
-            $showOnlineInformation = true;
-            $onlineinfo = [];
-
-            $onlineinfo['queue'] = $this->sessionStatus['queue'];
-            $onlineinfo['count'] = $this->sessionStatus['count'];
-            $onlineinfo['period'] = $this->sessionLimit['period'];
-        } 
-
-        // Show user information such as IP, user-agent, device name.
-        if (!empty($this->properties['display_user_info'])) {
-            $showUserInformation = true;
-            $dialoguserinfo = [];
-
-            $dialoguserinfo['ip'] = $this->ip;
-            $dialoguserinfo['rdns'] = $this->rdns;
-            $dialoguserinfo['user_agent'] = get_request()->getHeaderLine('user-agent');
-        }
-
-        // Captcha form
-        $form = $this->getCurrentUrl();
-        $captchas = $this->captcha;
-
-        $ui = [
-            'background_image' => '',
-            'bg_color'         => '#ffffff',
-            'header_bg_color'  => '#212531',
-            'header_color'     => '#ffffff',
-            'shadow_opacity'   => '0.2',
-        ];
-
-        foreach (array_keys($ui) as $key) {
-            if (!empty($this->dialog[$key])) {
-                $ui[$key] = $this->dialog[$key];
-            }
-        }
-
-        if (!defined('SHIELDON_VIEW')) {
-            define('SHIELDON_VIEW', true);
-        }
-
-        $css = include $this->getTemplate('css/default');
-
-        ob_start();
-        include $viewPath;
-        $output = ob_get_contents();
-        ob_end_clean();
-
-        // Remove unused variable notices generated from PHP intelephense.
-        unset(
-            $css,
-            $ui,
-            $form,
-            $captchas,
-            $langCode,
-            $showOnlineInformation,
-            $showUserInformation
-        );
-
-        $stream = HttpFactory::createStream();
-        $stream->write($output);
-        $stream->rewind();
-
-        return $response
-            ->withHeader('X-Protected-By', 'shieldon.io')
-            ->withBody($stream)
-            ->withStatus($statusCode);
-    }
-
-    /**
      * Ban an IP.
      *
      * @param string $ip A valid IP address.
@@ -642,53 +532,6 @@ class Kernel
     public function setDialog(array $settings): void
     {
         $this->dialog = $settings;
-    }
-
-    /**
-     * Set the frontend template directory.
-     *
-     * @param string $directory The directory in where the template files are placed.
-     *
-     * @return void
-     */
-    public function setTemplateDirectory(string $directory)
-    {
-        if (!is_dir($directory)) {
-            throw new InvalidArgumentException(
-                'The template directory does not exist.'
-            );
-        }
-        $this->templateDirectory = $directory;
-    }
-
-    /**
-     * Print a JavaScript snippet in your webpages.
-     * 
-     * This snippet generate cookie on client's browser,then we check the 
-     * cookie to identify the client is a rebot or not.
-     *
-     * @return string
-     */
-    public function getJavascript(): string
-    {
-        $tmpCookieName = $this->properties['cookie_name'];
-        $tmpCookieDomain = $this->properties['cookie_domain'];
-
-        if (empty($tmpCookieDomain) && get_request()->getHeaderLine('host')) {
-            $tmpCookieDomain = get_request()->getHeaderLine('host');
-        }
-
-        $tmpCookieValue = $this->properties['cookie_value'];
-
-        $jsString = '
-            <script>
-                var d = new Date();
-                d.setTime(d.getTime()+(60*60*24*30));
-                document.cookie = "' . $tmpCookieName . '=' . $tmpCookieValue . ';domain=.' . $tmpCookieDomain . ';expires="+d.toUTCString();
-            </script>
-        ';
-
-        return $jsString;
     }
 
     /**
---
+++ src/Firewall/Kernel/TemplateTrait.php
@@ -0,0 +1,213 @@
+<?php
+/**
+ * This file is part of the Shieldon package.
+ *
+ * (c) Terry L. <contact@terryl.in>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ * 
+ * php version 7.1.0
+ * 
+ * @category  Web-security
+ * @package   Shieldon
+ * @author    Terry Lin <contact@terryl.in>
+ * @copyright 2019 terrylinooo
+ * @license   https://github.com/terrylinooo/shieldon/blob/2.x/LICENSE MIT
+ * @link      https://github.com/terrylinooo/shieldon
+ * @see       https://shieldon.io
+ */
+
+declare(strict_types=1);
+
+namespace Shieldon\Firewall\Kernel;
+
+use Psr\Http\Message\ResponseInterface;
+use Shieldon\Firewall\Kernel;
+use Shieldon\Firewall\HttpFactory;
+use function Shieldon\Firewall\get_response;
+use function Shieldon\Firewall\get_request;
+use function Shieldon\Firewall\get_session;
+
+use InvalidArgumentException;
+use function array_keys;
+use function define;
+use function defined;
+use function is_dir;
+use function ob_end_clean;
+use function ob_get_contents;
+use function ob_start;
+
+/*
+ * The template-related functions.
+ */
+trait TemplateTrait
+{
+    /**
+     *   Public methods       | Desctiotion
+     *  ----------------------|---------------------------------------------
+     *   respond              | Respond the result.
+     *   setTemplateDirectory | Set the frontend template directory.
+     *   getJavascript        | Print a JavaScript snippet in the pages.
+     *  ----------------------|---------------------------------------------
+     */
+
+    /**
+     * Respond the result.
+     *
+     * @return ResponseInterface
+     */
+    public function respond(): ResponseInterface
+    {
+        $response = get_response();
+
+        $httpStatusCodes = [
+            kernel::RESPONSE_TEMPORARILY_DENY => [
+                'type' => 'captcha',
+                'code' => kernel::HTTP_STATUS_FORBIDDEN,
+            ],
+
+            kernel::RESPONSE_LIMIT_SESSION => [
+                'type' => 'session_limitation',
+                'code' => kernel::HTTP_STATUS_TOO_MANY_REQUESTS,
+            ],
+
+            kernel::RESPONSE_DENY => [
+                'type' => 'rejection',
+                'code' => kernel::HTTP_STATUS_BAD_REQUEST,
+            ],
+        ];
+
+        // Nothing happened. Return.
+        if (empty($httpStatusCodes[$this->result])) {
+            return $response;
+        }
+
+        $type = $httpStatusCodes[$this->result]['type'];
+        $statusCode = $httpStatusCodes[$this->result]['code'];
+
+        $viewPath = $this->getTemplate($type);
+
+        // The language of output UI. It is used on views.
+        $langCode = get_session()->get('shieldon_ui_lang') ?? 'en';
+
+        $showOnlineInformation = false;
+        $showUserInformation = false;
+        
+        // Show online session count. It is used on views.
+        if (!empty($this->properties['display_online_info'])) {
+            $showOnlineInformation = true;
+            $onlineinfo = [];
+
+            $onlineinfo['queue'] = $this->sessionStatus['queue'];
+            $onlineinfo['count'] = $this->sessionStatus['count'];
+            $onlineinfo['period'] = $this->sessionLimit['period'];
+        } 
+
+        // Show user information such as IP, user-agent, device name.
+        if (!empty($this->properties['display_user_info'])) {
+            $showUserInformation = true;
+            $dialoguserinfo = [];
+
+            $dialoguserinfo['ip'] = $this->ip;
+            $dialoguserinfo['rdns'] = $this->rdns;
+            $dialoguserinfo['user_agent'] = get_request()->getHeaderLine('user-agent');
+        }
+
+        // Captcha form
+        $form = $this->getCurrentUrl();
+        $captchas = $this->captcha;
+
+        $ui = [
+            'background_image' => '',
+            'bg_color'         => '#ffffff',
+            'header_bg_color'  => '#212531',
+            'header_color'     => '#ffffff',
+            'shadow_opacity'   => '0.2',
+        ];
+
+        foreach (array_keys($ui) as $key) {
+            if (!empty($this->dialog[$key])) {
+                $ui[$key] = $this->dialog[$key];
+            }
+        }
+
+        if (!defined('SHIELDON_VIEW')) {
+            define('SHIELDON_VIEW', true);
+        }
+
+        $css = include $this->getTemplate('css/default');
+
+        ob_start();
+        include $viewPath;
+        $output = ob_get_contents();
+        ob_end_clean();
+
+        // Remove unused variable notices generated from PHP intelephense.
+        unset(
+            $css,
+            $ui,
+            $form,
+            $captchas,
+            $langCode,
+            $showOnlineInformation,
+            $showUserInformation
+        );
+
+        $stream = HttpFactory::createStream();
+        $stream->write($output);
+        $stream->rewind();
+
+        return $response
+            ->withHeader('X-Protected-By', 'shieldon.io')
+            ->withBody($stream)
+            ->withStatus($statusCode);
+    }
+
+    /**
+     * Print a JavaScript snippet in your webpages.
+     * 
+     * This snippet generate cookie on client's browser,then we check the 
+     * cookie to identify the client is a rebot or not.
+     *
+     * @return string
+     */
+    public function getJavascript(): string
+    {
+        $tmpCookieName = $this->properties['cookie_name'];
+        $tmpCookieDomain = $this->properties['cookie_domain'];
+
+        if (empty($tmpCookieDomain) && get_request()->getHeaderLine('host')) {
+            $tmpCookieDomain = get_request()->getHeaderLine('host');
+        }
+
+        $tmpCookieValue = $this->properties['cookie_value'];
+
+        $jsString = '
+            <script>
+                var d = new Date();
+                d.setTime(d.getTime()+(60*60*24*30));
+                document.cookie = "' . $tmpCookieName . '=' . $tmpCookieValue . ';domain=.' . $tmpCookieDomain . ';expires="+d.toUTCString();
+            </script>
+        ';
+
+        return $jsString;
+    }
+
+    /**
+     * Set the frontend template directory.
+     *
+     * @param string $directory The directory in where the template files are placed.
+     *
+     * @return void
+     */
+    public function setTemplateDirectory(string $directory)
+    {
+        if (!is_dir($directory)) {
+            throw new InvalidArgumentException(
+                'The template directory does not exist.'
+            );
+        }
+        $this->templateDirectory = $directory;
+    }
+}
