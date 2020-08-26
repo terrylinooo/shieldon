677e7818db02ab4f6f5a6e37a74b030c1b2f66dc
Viktor Szépe <viktor@szepe.net>
2020-08-26 14:08:08 +0200

    Fix microtimesTamp

--- src/Firewall/Driver/SqlDriverProvider.php
+++ src/Firewall/Driver/SqlDriverProvider.php
@@ -487,7 +487,7 @@ class SqlDriverProvider extends DriverProvider
                     `id` varchar(40) NOT NULL,
                     `ip` varchar(46) NOT NULL,
                     `time` int(10) UNSIGNED NOT NULL,
-                    `microtimesamp` bigint(20) UNSIGNED NOT NULL,
+                    `microtimestamp` bigint(20) UNSIGNED NOT NULL,
                     `data` blob,
                     PRIMARY KEY (`id`)
                 ) ENGINE={$this->tableDbEngine} DEFAULT CHARSET=latin1;
